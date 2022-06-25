68c3a1696e97b643c6296b5174a4abe93fec2881
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-25 15:54:10 +0530

    [Tushar Vidwans] Creating table AddressBook for AddressBookService DataBase

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -1,3 +1,18 @@
 /*UC1 - Creating AddressBookService DataBase*/
 
 create database AddressBookService
+
+/*UC2 - Creating table */
+
+create table AddressBook(
+firstName varchar(255) not null,
+lastname varchar(255) not null,
+Address varchar(255) not null,
+City varchar(255) not null,
+State varchar(255) not null,
+Zip int not null,
+PhoneNumber int,
+email varchar(255)
+
+)
+
