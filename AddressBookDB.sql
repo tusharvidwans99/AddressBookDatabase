26dbe6516d88e2cb4a4e8625e494121f2ea93f37
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-25 16:55:51 +0530

    [Tushar Vidwans] Inserting data into AddressBook table

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -11,8 +11,16 @@ Address varchar(255) not null,
 City varchar(255) not null,
 State varchar(255) not null,
 Zip int not null,
-PhoneNumber int,
+PhoneNumber bigint,
 email varchar(255)
-
 )
 
+/*UC3 - Inserting data into AddressBook table*/
+insert into AddressBook (firstName,lastname,Address,City,State,Zip,PhoneNumber,email) values('Tushar','Vidwans','Malkapur','Akola','Maharashtra',444002,9096486283,'tusharvidwans99@gmail.com')
+insert into AddressBook values
+('Saran','Yellanki','Kurmannapalem','Vizag','Andhra Pradesh',530046,9618058892,'saran.yellanki12@gmail.com'),
+('Shubham','Verma','Gachibowli','Hyderabad','Telangana',520017,9775638892,'shubham.verma@yahoo.co.in'),
+('Madhukar','Tatiraju','Gurudwar','Vizag','Andhra Pradesh',530032,9347665123,'mad.t@gmail.com'),
+('Basila','Noushad','Mattancherry','Kochi','Kerala',510033,9688920910,'basila.noushad@yahoo.com'),
+('Manasa','Pochiraju','Steel Plant','Vizag','Andhra Pradesh',530032,9848553322,'manasa.p@yahoo.com'),
+('Sai','Chandu','Chanda Nagar','Hyderabad','Telangana',540081,9909339211,'saichandu998@gmail.com')
